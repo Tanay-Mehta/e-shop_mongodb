d2fc78e7c55ac69f4bace2a825898b5a990e91d4
Tanay Mehta <mehtatanay30@gmail.com>
2024-06-11 08:29:15 +0530

    transaction table

--- play.py
+++ play.py
@@ -6,8 +6,8 @@ prod_aval = db['products in stock']
 transaction = db['products sold']
 
 
-insert_cond = input('do you want to do insert, delete or nothing "say i , d or n": ')
-if insert_cond == 'i':
+insert_cond = input('do you want to do restore stock, sale or exit "say r , s or e": ')
+if insert_cond == 'r':
     name = (input("which product: ")).lower().strip()
     qnt = int(input("quantity: "))
     d = prod_aval.find_one({"name":name})
@@ -16,7 +16,7 @@ if insert_cond == 'i':
         prod_aval.update_one({"name":name}, {"$set" :{"qnt": qnt + d_qnt}})
     if d==None:
         prod_aval.insert_one({"name": name, "qnt": qnt})
-elif insert_cond == "d":
+elif insert_cond == "s":
     name = (input("which product: ")).lower().strip()
     qnt = int(input("quantity: "))
     d = prod_aval.find_one({"name": name})
@@ -24,10 +24,21 @@ elif insert_cond == "d":
         threshold_q = d["qnt"]
         if threshold_q > qnt:
             prod_aval.update_one({"name": name}, {"$set": {"qnt": threshold_q - qnt}})
+            n = transaction.find_one({"name": name})
+            if n == None:
+                transaction.insert_one({"name": name, "qnt": qnt})
+            else:
+                n_qnt = n["qnt"]
+                transaction.update_one({"name": name}, {"$set": {"qnt": qnt + n_qnt}})
         elif threshold_q == qnt:
             prod_aval.delete_one({"name": name})
+            n = transaction.find_one({"name": name})
+            if n == None:
+                transaction.insert_one({"name": name, "qnt": qnt})
+            else:
+                n_qnt = n["qnt"]
+                transaction.update_one({"name": name}, {"$set": {"qnt": qnt + n_qnt}})
         else:
             print(f"this is not possible there are only {threshold_q} {d['name']} available")
-
 
 
